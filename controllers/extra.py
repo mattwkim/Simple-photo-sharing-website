4bd8490a0b5845bfdd32211672b375f1de2a1755
vagrant <vagrant@vagrant.vm>
2016-09-18 22:13:58 +0000

    Initil commit

--- controllers/extra.py
+++ controllers/extra.py
@@ -8,26 +8,13 @@ main = Blueprint('main', __name__, template_folder='templates')
 def main_route():
     return render_template('index.html')
 
-def connect_to_database():
-  options = {
-    'host': 'localhost',
-    'user': 'root',
-    'passwd': 'root',
-    'db': 'test_db',
-    'cursorclass' : MySQLdb.cursors.DictCursor
-  }
-  db = MySQLdb.connect(**options)
-  db.autocommit(True)
-  return db
-
-@main.route('/hello')
+@main.route('/test_database')
 def test_route():
   db = connect_to_database()
   cur = db.cursor()
-  cur.execute('SELECT id, name FROM test_tbl')
+  cur.execute('SELECT username FROM User')
   results = cur.fetchall()
   return_string = ''
   for result in results:
-    return_string += str(result['id']) + ' ' + str(result['name']) + '\n'
+    return_string += str(result['username']) + '\n'
   return return_string
-

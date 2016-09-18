dc1fc0175236fafa201b91d5ea924423d62db346
Gary Johnson <garyjohn@umich.edu>
2016-09-18 18:40:47 -0400

    Delete tbl_creates.sql

--- sql/tbl_creates.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE User (username VARCHAR(20) NOT NULL, firstname VARCHAR(20), lastname VARCHAR(20), password VARCHAR(20), email VARCHAR(40), PRIMARY KEY (username));
-
-CREATE TABLE Album (albumid INT NOT NULL AUTO_INCREMENT, title VARCHAR(50), created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, lastupdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP, username VARCHAR(20), PRIMARY KEY (albumid));
-
-CREATE TABLE Contain (sequencenum INTEGER DEFAULT '0', albumid INT NOT NULL, picid VARCHAR(40), caption VARCHAR(255), PRIMARY KEY(sequencenum));
-
-CREATE TABLE Photo(picid VARCHAR(40) NOT NULL, format CHAR(3), date TIMESTAMP, PRIMARY KEY(picid));
